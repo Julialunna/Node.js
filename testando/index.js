83f68f8c224070c5259615fbdc6251f63d065c7e
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-04 15:23:18 -0300

    usando lib fs

--- testando/index.js
+++ testando/index.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
 import chalk from 'chalk';
-console.log(chalk.blue('Olá, mundo!'));
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
+function trataErro(erro){
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+//Promessa com then
+/*function pegaArquivo(caminhoDoArquivo){
+    const encoding='utf-8';
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto)=>conaole.log(chalk.green(texto)))
+    .catch(trataErro);
+}*/
+
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding='utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    }catch(erro){
+        trataErro(erro);
+    }
+}
+
+//pegaArquivo('./arquivos/');
+pegaArquivo('./arquivos/texto.md');
