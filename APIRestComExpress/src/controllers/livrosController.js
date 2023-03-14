41c06259fe21512be413a5a7759e312f7d3ef00d
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-14 15:55:52 -0300

    terminando projeto

--- APIRestComExpress/src/controllers/livrosController.js
+++ APIRestComExpress/src/controllers/livrosController.js
@@ -3,10 +3,38 @@ import livros from '../models/Livro.js';
 class LivroController {
 
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
+    livros.find()
+    .populate('autor')
+    .exec((err, livros) => {
       res.status(200).json(livros);
     });
   }; 
+
+  static listarLivroPorId = (req, res) => {
+    const id= req.params.id;
+
+    livros.findById(id)
+    .populate('autor', 'nome')
+    .exec((err, livros)=>{
+        if(err){
+          res.status(400).send({message: `${err.message} -  id do livro não localizado`});
+        }else{
+          res.status(200).send(livros);
+        }
+    })
+  }
+
+  static listarLivroPorEditora = (req, res) => {
+    const editora = req.query.editora;
+      livros.find({'editora': editora}, {}, (err, livros)=>{
+        if(err){
+          res.status(400).send({message: `${err.message} -  não foi possível localizar livro`});
+        }else{
+          res.status(200).send(livros);
+        }
+      })
+  }
+
 
   static cadastrarLivro = (req,res) => {
     let livro= new livros(req.body);
@@ -19,16 +47,28 @@ class LivroController {
     })
   }
 
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+    livros.findByIdAndUpdate(id, {$set: req.body}, (err)=>{
+      if(!err){
+        res.status(200).send({message:' Livro atualizado com sucesso'});
+      }else{
+        res.status(500).send({message: err.message})
+      }
+    });
+  }
+
+ 
   static excluirLivro = (req, res) => {
     const id=req.params.id;
 
-    livros.findByIdAndDelete(id), (err)=>{
+    livros.findByIdAndDelete(id, (err)=>{
         if(!err){
-            res.status(200).send({message:'Livro excluído com sucesso'})
+            res.status(200).send({message:'Livro removido com sucesso'})
         }else{
             res.status(500).send({message:err.message});
         }
-    };
+    });
   }
 }
 export default LivroController;
