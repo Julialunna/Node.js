56dbaf7e7b3e236c24a5aa906517f33b3d85f822
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-18 12:04:25 -0300

    criando rotas e controllers de níveis e turmas

--- ORMComNodeJs/APIComSequelizeEMySQL/api/routes/index.js
+++ ORMComNodeJs/APIComSequelizeEMySQL/api/routes/index.js
@@ -1,7 +1,13 @@
 const bodyParser = require('body-parser');
 const pessoas = require('./pessoasRoute.js');
+const turmas = require('./turmasRoute.js');
+const niveis = require('./niveisRoute.js')
 
-module.exports = app =>{
-    app.use(bodyParser.json());
-    app.use(pessoas)
+module.exports = app => {
+    app.use(bodyParser.json(),
+    bodyParser.urlencoded({ extended: false }),
+    pessoas, 
+    turmas, 
+    niveis
+    )
 }
