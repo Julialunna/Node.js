bb8821a2e1cb4a4ce10146c71c8e3d3abf96f937
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-17 16:53:35 -0300

    validações mongoose

--- APIRestComExpress2/src/models/Livro.js
+++ APIRestComExpress2/src/models/Livro.js
@@ -2,27 +2,33 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
   {
-    id: {type: String},
+    id: { type: String },
     titulo: {
-      type: String, 
-      required:[true, "O título do livro é obrigatório"] 
+      type: String,
+      required: [true, "O título do livro é obrigatório"]
     },
-    autor: {type: mongoose.Schema.Types.ObjectId, 
-      ref: "autores", 
+    autor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores",
       required: [true, "O nome do(a)  autor(a) é obrigatório"]
     },
-    editora: {type: String, 
-      required: [true, "O nome da editora é obrigatório"], 
-      enum: {values:["Casa do Código", "Alura"], message:"A editora {VALUE} não é um valor permitido"}
+    editora: {
+      type: String,
+      required: [true, "O nome da editora é obrigatório"],
+      enum: { values: ["Casa do Código", "Alura"], message: "A editora {VALUE} não é um valor permitido" }
     },
     numeroPaginas: {
       type: Number,
-      min:[10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
-      max:[5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
     }
   }
 );
 
-const livros= mongoose.model("livros", livroSchema);
+const livros = mongoose.model("livros", livroSchema);
 
 export default livros;
