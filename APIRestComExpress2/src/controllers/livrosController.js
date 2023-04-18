8022165d3152c6081f2e77088f1c15c3bd9d7841
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-18 14:06:03 -0300

    paginação e ordenando resultados

--- APIRestComExpress2/src/controllers/livrosController.js
+++ APIRestComExpress2/src/controllers/livrosController.js
@@ -1,14 +1,28 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
+import ResquisicaoIncorreta from "../erros/ResquisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      const livrosResultado = await livros.find()
-        .populate("autor")
-        .exec();
-      res.status(200).json(livrosResultado);
+      let {limite=5, pagina=1, ordenacao="_id:-1"}=req.query;
+      let [campoOrdenacao, ordem]=ordenacao.split(":");
+      limite=parseInt(limite);
+      pagina=parseInt(pagina);
+      ordem=parseInt(ordem);
+      if(limite>0&&pagina>0){
+
+        const livrosResultado = await livros.find()
+          .sort({[campoOrdenacao]:ordem})
+          .skip((pagina-1)*limite)
+          .limit(limite)
+          .populate("autor")
+          .exec();
+        res.status(200).json(livrosResultado);
+      }else{
+        next(new ResquisicaoIncorreta());
+      }
     } catch (erro) {
       next(erro);
     }
